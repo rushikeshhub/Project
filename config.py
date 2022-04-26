f63f1e7047b9f436715dd3dbe4a459ddf815c93e
rushikeshhub <rushikesh.mhaske10@gmail.com>
2022-04-26 16:49:41 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cnmmb1xzwoxd.ap-south-1.rds.amazonaws.com"
+customuser = "Rushikesh"
+custompass = "rushi1990"
+customdb = "employee"
+custombucket = "addemployeeinstorage"
+customregion = "ap-south-1b"
